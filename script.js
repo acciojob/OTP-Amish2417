5b772706bb3bb39e70e641711bde22adfdba65e8
ANISH KUMAR <70019437+Amish2417@users.noreply.github.com>
2024-04-23 07:29:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let input1 = document.querySelector('#singleDigitInput');
+input1.addEventListener('keypress',event => {
+	 if (event.key.length > 1) {
+    event.preventDefault();
+		 console.log('hiii');
+  }
+})
